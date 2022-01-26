f901060cd006abffdee5c78eb62f4afe4a792664
Dmitry <d@d.d>
2022-01-27 02:50:56 +0300

    ref

---
+++ Blazor.JsBindingsGenerator/BlazorJsBindingsSourceGenerator.ConsumerAttributesDefinition.cs
@@ -0,0 +1,37 @@
+﻿namespace JsBindingsGenerator;
+
+public partial class BlazorJsBindingsSourceGenerator
+{
+    private const string Namespace = nameof(JsBindingsGenerator);
+    private const string JsBindingContextAttribute = "JsBindingContextAttribute";
+    private const string JsBindAttribute = "JsBindAttribute";
+    private const string Params = "Params";
+    private const string Returns = "Returns";
+    private const string ResetContext = "ResetContext";
+
+    private const string AttributesToUse = $@"// Auto-generated
+#nullable enable
+
+using System;
+
+namespace {Namespace};
+
+[AttributeUsage(AttributeTargets.Class)]
+internal class {JsBindingContextAttribute} : Attribute
+{{
+    public {JsBindingContextAttribute}(string jsContext) {{}}
+}}
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+internal class {JsBindAttribute} : Attribute
+{{
+    public Type? {Params} {{ get; init; }}
+
+    public Type {Returns} {{ get; init; }} = typeof(void);
+
+    public bool {ResetContext} {{ get; init; }}
+
+    public {JsBindAttribute}(string member) {{}}
+}}
+";
+}
