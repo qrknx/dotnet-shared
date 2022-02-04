577ed8e3c0e05b5eb3a81836cc2d788cddc7c2ce
Dmitry <d@d.d>
2022-02-04 07:12:16 +0300

    test with alias

---
+++ Blazor.JsBindingsGenerator.Tests/JsBindingsTestData/Signature_FullNameResolution/JsBindings.g.cs
@@ -0,0 +1,27 @@
+﻿// Auto-generated
+#nullable enable
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace A
+{
+    public static partial class B
+    {
+        public static async Task VisibleNamespaceAsync(this IJSRuntime js, global::System.Collections.IList list, int i, CancellationToken token)
+        {
+            await js.InvokeVoidAsync("VisibleNamespaceAsync", token, list, i);
+        }
+
+        public static async Task WithAliasAsync(this IJSRuntime js, short s, global::System.IDisposable d, CancellationToken token)
+        {
+            await js.InvokeVoidAsync("WithAliasAsync", token, s, d);
+        }
+
+        public static async Task FullyQualifiedAsync(this IJSRuntime js, global::CustomNamespace.Nested.CustomStruct s, CancellationToken token)
+        {
+            await js.InvokeVoidAsync("FullyQualifiedAsync", token, s);
+        }
+    }
+}
