7868f8c034a302046eb5b5de2db1a8567420e789
Dmitry <d@d.d>
2022-02-07 16:59:22 +0300

    DotNetToolbox

---
+++ DotNetToolbox/EnumerableExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace DotNetToolbox;
+
+public static class EnumerableExtensions
+{
+    public static List<TResult> ToList<TSource, TResult>(this IEnumerable<TSource> source,
+                                                         Func<TSource, int, TResult> selector)
+    {
+        return source.Select(selector)
+                     .ToList();
+    }
+
+    public static List<TResult> ToList<TSource, TResult>(this IEnumerable<TSource> source,
+                                                         Func<TSource, TResult> selector)
+    {
+        return source.Select(selector)
+                     .ToList();
+    }
+
+    public static TResult[] ToArray<TSource, TResult>(this IEnumerable<TSource> source,
+                                                      Func<TSource, int, TResult> selector)
+    {
+        return source.Select(selector)
+                     .ToArray();
+    }
+
+    public static TResult[] ToArray<TSource, TResult>(this IEnumerable<TSource> source,
+                                                      Func<TSource, TResult> selector)
+    {
+        return source.Select(selector)
+                     .ToArray();
+    }
+}
